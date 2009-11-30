0939eb679bf383e21a8fcf9d19cf1c0fc11cfae6
felix <felix@1aa16b1b-5976-5682-e511-4c0f6b60a7af>
2009-11-30 18:05:57 +0000

    Version 0.9: Erster Import, funktioniert Keine Bugs bekannt Todo: - en-EN locale - Icons austauschen

---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,7 @@
+pref("extensions.hidebookmarksbar.startup", 2);
+pref("extensions.hidebookmarksbar.visible", true);
+
+pref("extensions.hidebookmarksbar.shortcut.enabled", true);
+pref("extensions.hidebookmarksbar.shortcut.modifiers", "accel,alt");
+pref("extensions.hidebookmarksbar.shortcut.key", "L");
+
