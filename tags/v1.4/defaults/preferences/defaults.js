7f47bdf62cabd383e0d29328197c00e26ce689f5
felix.kloft <felix.kloft@1aa16b1b-5976-5682-e511-4c0f6b60a7af>
2011-05-15 17:11:03 +0000

    Created v1.4

---
+++ tags/v1.4/defaults/preferences/defaults.js
@@ -0,0 +1,8 @@
+pref("extensions.hidebookmarksbar.startup", 2);
+pref("extensions.hidebookmarksbar.popup", true);
+pref("extensions.hidebookmarksbar.visible", true);
+
+pref("extensions.hidebookmarksbar.shortcut.enabled", true);
+pref("extensions.hidebookmarksbar.shortcut.modifiers", "accel,alt");
+pref("extensions.hidebookmarksbar.shortcut.key", "L");
+
