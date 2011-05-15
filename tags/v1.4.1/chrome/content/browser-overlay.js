8bed47b6a385cdcec6a6bb892fed663b3edbef3d
felix.kloft <felix.kloft@1aa16b1b-5976-5682-e511-4c0f6b60a7af>
2011-05-15 17:11:19 +0000

    Created v1.4.1

---
+++ tags/v1.4.1/chrome/content/browser-overlay.js
@@ -0,0 +1,76 @@
+var hidebookmarksbar = 
+{
+	prefs: null,
+	visible: true,
+	
+	onLoad: function()
+	{
+		this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+		.getService(Components.interfaces.nsIPrefService)
+		.getBranch("extensions.hidebookmarksbar.");
+		this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+		this.prefs.addObserver("", this, false);
+		
+		var key = document.getElementById("hidebookmarksbarKeyset");
+		key.setAttribute("modifiers", this.prefs.getCharPref("shortcut.modifiers"));
+		key.setAttribute("key",       this.prefs.getCharPref("shortcut.key"));
+		key.setAttribute("disabled", !this.prefs.getBoolPref("shortcut.enabled"));
+		
+		var startup = this.prefs.getIntPref("startup");
+		if(startup < 2)
+			this.visible = !!startup;
+		else
+			this.visible = this.prefs.getBoolPref("visible");
+		
+		this.setMode();
+		
+		var popup = document.getElementById("hidebookmarksbarButton");
+		popup.type = this.prefs.getBoolPref("popup") ? "menu-button" : "button";
+	},
+	
+	onUnload: function()
+	{
+		this.prefs.removeObserver("", this);
+	},
+	
+	observe: function(subject, topic, data)
+	{
+		if (topic != "nsPref:changed")
+		{
+			return;
+		}
+		
+		switch(data)
+		{
+			case "visible":
+				this.visible = this.prefs.getBoolPref(data);
+				this.setMode();
+				break;
+			
+			case "popup":
+				var popup = document.getElementById("hidebookmarksbarButton");
+				popup.type = this.prefs.getBoolPref(data) ? "menu-button" : "button";
+				break;
+		}
+	},
+	
+	toggle: function()
+	{
+		this.prefs.setBoolPref("visible", !this.visible);
+	},
+	
+	setMode: function()
+	{
+		var toolbar = document.getElementById("PersonalToolbar");
+		toolbar.collapsed = !this.visible;
+	},
+	
+	openOptions: function()
+	{
+		window.open("chrome://hidebookmarksbar/content/options.xul", "hidebooksmarksoptions", "chrome, dialog, centerscreen, alwaysRaised")
+	}
+};
+
+window.addEventListener("load",   function(e){hidebookmarksbar.onLoad();  }, false);
+window.addEventListener("unload", function(e){hidebookmarksbar.onUnload();}, false);
+
